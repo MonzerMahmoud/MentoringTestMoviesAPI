6ffea98a19d3955000abc0440bec154c148ab162
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2021-10-05 18:01:45 +0200

    Adde Movie Model

---
+++ MentoringTestMoviesAPI/Model/MoviesModel.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesModel.swift
+//  MentoringTestMoviesAPI
+//
+//  Created by Syber Expertise on 04/10/2021.
+//
+
+import Foundation
+
+struct MoviesModel:Decodable {
+    var imdb_id: String
+    var title: String
+}
